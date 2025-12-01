ccccba4a038fc7a48bd96e8b441d9e01ac36d284
gaurav.arora <gauravarora.epri@gmail.com>
2025-12-01 11:54:26 +0530

    java practice Dec1

---
+++ src/interview_preparation_array_and_string_coding_set2/LongestSubStringWithoutRepeatingCharacter.java
@@ -0,0 +1,45 @@
+package interview_preparation_array_and_string_coding_set2;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class LongestSubStringWithoutRepeatingCharacter
+
+{
+
+    static void main() {
+        String str="hhhhhhhijklmnoabcdefghhhhhhhijklmno";
+        Set<String> subStringSet=new LinkedHashSet<>();
+        for(int i=0;i<str.length();i++)
+        {
+            String subString=str.charAt(i)+"";
+            for(int j=i+1;j<str.length();j++)
+            {
+                if(subString.charAt(subString.length()-1)!=str.charAt(j))
+                {
+                        subString=subString+str.charAt(j);
+                        i++;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+            subStringSet.add(subString);
+        }
+        System.out.println("Substring set is: "+subStringSet);
+        String longestString=subStringSet.iterator().next();
+        for(String temp:subStringSet)
+        {
+            if(temp.length()>longestString.length())
+            {
+                longestString=temp;
+            }
+        }
+        System.out.println("Longest string is: "+longestString);
+
+    }
+
+}
---
+++ src/interview_preparation_array_and_string_coding_set2/ReverseWordsInASentence.java
@@ -0,0 +1,28 @@
+package interview_preparation_array_and_string_coding_set2;
+
+import java.util.Arrays;
+
+public class ReverseWordsInASentence
+
+{
+
+    static void main() {
+        String sentence="Hi this is Gaurav Arora";
+        String ar[]=sentence.split("\\s");
+        String reverse_sentence="";
+        System.out.println("Original sentence is: "+sentence);
+        for(int i=0;i<ar.length;i++)
+        {
+            String subString="";
+            for(int j=ar[i].length()-1;j>=0;j--)
+            {
+                subString=subString+ar[i].charAt(j);
+            }
+            reverse_sentence=reverse_sentence+subString+" ";
+
+        }
+        System.out.println("Reverse sentence is: "+reverse_sentence);
+
+    }
+
+}
